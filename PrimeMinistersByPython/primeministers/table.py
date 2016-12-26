6a283e3c0c640091e3f9ec05116aae669a104845
miyazakisoft <special.research2016@gmail.com>
2016-12-26 19:37:27 +0900

    Javaを途中まで進めた

--- PrimeMinistersByPython/primeministers/table.py
+++ PrimeMinistersByPython/primeministers/table.py
@@ -8,14 +8,48 @@ class Table(object):
 
 	def __init__(self, kind_string):
 		"""テーブルのコンストラクタ。"""
+
+
+		if kind_string == 'input':
+				####
+				##
+		else :
+			
+
+
+
+		self._kind_string = kind_string
+
+		self._images = []
+		self._thumbnails = []
+		self._tuples = []
+
 		return
 
 	def __str__(self):
 		"""自分自身を文字列にして、それを応答する。"""
-		return None
+
+		respondString = self.__class__.__name__
+
+
+
+		return respondString
 
 	def add(self, tuple):
 		"""タプルを追加する。"""
+
+		tupleValues = tuple.values()
+		tupleKeys = tuple.attributes().keys()
+
+
+		#if values[tupleKeys.index('image')] != '画像':
+		#	self._images.append(values[keys.index('image')])
+		#	if self._kind_string == 'input':
+		#		self._thumbnails.append(values[keys.index('thumbnail')])
+
+		self._tuples.append(tuple)
+
+
 		return
 
 	def attributes(self):
@@ -24,12 +58,15 @@ class Table(object):
 
 	def image_filenames(self):
 		"""画像ファイル群をリストにして応答する。"""
-		return None
+		return self._images
 
 	def thumbnail_filenames(self):
 		"""縮小画像ファイル群をリストにして応答する。"""
-		return None
-	
+
+
+
+		return self._thumbnails
+
 	def tuples(self):
 		"""タプル群を応答する。"""
 		return None
