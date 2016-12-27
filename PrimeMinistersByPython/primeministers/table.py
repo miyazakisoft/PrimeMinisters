f7b5d8808fa6deeebd4b856f30a925a974c2476a
miyazakisoft <special.research2016@gmail.com>
2016-12-27 22:11:41 +0900

    頑張って書いた

--- PrimeMinistersByPython/primeministers/table.py
+++ PrimeMinistersByPython/primeministers/table.py
@@ -8,53 +8,39 @@ class Table(object):
 
 	def __init__(self, kind_string):
 		"""テーブルのコンストラクタ。"""
-
-
-		if kind_string == 'input':
-				####
-				##
-		else :
-			
-
-
-
-		self._kind_string = kind_string
-
-		self._images = []
-		self._thumbnails = []
-		self._tuples = []
+        self._kind_string = kind_string
+        self._attributes = attributes.Attributes(kind_string)
+        self._images = []
+        self._thumbnails = []
+        self._tuples = []
 
 		return
 
 	def __str__(self):
 		"""自分自身を文字列にして、それを応答する。"""
-
-		respondString = self.__class__.__name__
-
-
-
-		return respondString
+        a_string = self.__class__.__name__
+        a_string += "\n"
+        a_string += "Attributes:"
+        a_string += ",".join(self._attributes)
+        a_string += "images:"
+        a_string += ",".join(self._images)
+        a_string += "thumbnails:"
+        a_string += ",".join(self._thumbnails)
+        a_string += "images:"
+        a_string += ",".join(self._tuples)
+		return
 
 	def add(self, tuple):
 		"""タプルを追加する。"""
-
-		tupleValues = tuple.values()
-		tupleKeys = tuple.attributes().keys()
-
-
-		#if values[tupleKeys.index('image')] != '画像':
-		#	self._images.append(values[keys.index('image')])
-		#	if self._kind_string == 'input':
-		#		self._thumbnails.append(values[keys.index('thumbnail')])
-
-		self._tuples.append(tuple)
-
-
+        # values = tuple.values()
+        # keys = tuple.attributes().keys()
+        # if values[keys.index('image')] != '画像':
+        self._tuples.append(tuple)
 		return
 
 	def attributes(self):
 		"""属性リストを応答する。"""
-		return None
+		return self._attributes
 
 	def image_filenames(self):
 		"""画像ファイル群をリストにして応答する。"""
@@ -62,11 +48,8 @@ class Table(object):
 
 	def thumbnail_filenames(self):
 		"""縮小画像ファイル群をリストにして応答する。"""
-
-
-
 		return self._thumbnails
 
 	def tuples(self):
 		"""タプル群を応答する。"""
-		return None
+		return self._tuples
