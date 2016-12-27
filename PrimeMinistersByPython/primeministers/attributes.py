4ba100d91ae777d0a12c0171f8ea69f0c916a60c
syou4 <g1444693@gmail.com>
2016-12-27 23:44:52 +0900

    attribute.pyを微妙に修正してみた
    授業中に作業した分。
    正しいか分からないが、修正してみた。

--- PrimeMinistersByPython/primeministers/attributes.py
+++ PrimeMinistersByPython/primeministers/attributes.py
@@ -6,20 +6,22 @@ class Attributes(object):
 
 	def __init__(self, kind_string):
 		"""入力用("input")または出力用("output")で属性リストを作成するコンストラクタ。"""
+		
 		return
 
 	def __str__(self):
 		"""自分自身を文字列にして、それを応答する。"""
-		return None
+		return str(self)
 
 	def keys(self):
 		"""キー群を応答する。"""
-		return None
+		return keys
 
 	def names(self):
 		"""名前群を応答する。"""
-		return None
+		return names
 
 	def set_names(self, names):
 		"""名前群を設定する。"""
+		self.names = names
 		return
