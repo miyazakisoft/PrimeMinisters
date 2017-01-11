bca5c0b8eecbec6feb59a1b1628369c508967c7c
miyazakisoft <special.research2016@gmail.com>
2017-01-12 03:29:32 +0900

    提出フォーマットを統一

--- PrimeMinistersByPython/PrimeMinisters.app/Contents/Resources/Python/PrimeMinisters.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env python
-# -*- coding: utf-8 -*-
-
-import sys
-import primeministers.example
-
-if __name__ == "__main__":
-	an_example = primeministers.example.Example()
-	sys.exit(an_example.main())
